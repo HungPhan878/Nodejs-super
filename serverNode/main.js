44c6788e22a43991ddb4a294aab012115add4c79
Phan Van Hung <Hungphanhung8@gmail.com>
2024-08-04 20:44:08 +0700

    Init a server with nodejs

---
+++ serverNode/main.js
@@ -0,0 +1,25 @@
+const http = require('http');
+
+// Định nghĩa cổng
+const port = 3000;
+
+// Tạo server
+const server = http.createServer((req, res) => {
+  // Đặt tiêu đề HTTP
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'application/json');
+
+  // Tạo một đối tượng JSON
+  const responseObject = {
+    message: 'Hello, World!',
+    success: true,
+  };
+
+  // Trả về chuỗi JSON
+  res.end(JSON.stringify(responseObject));
+});
+
+// Khởi động server
+server.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}/`);
+});
