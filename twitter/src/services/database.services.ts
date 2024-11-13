a37561aee1695f6d0e72e1aec41d5688515c9938
Phan Van Hung <Hungphanhung8@gmail.com>
2024-11-13 09:56:04 +0700

    Twitter: Create schema and type for collection users p1

--- twitter/src/services/database.services.ts
+++ twitter/src/services/database.services.ts
@@ -1,30 +1,37 @@
-import { MongoClient } from 'mongodb'
+import { Collection, MongoClient } from 'mongodb'
 import { config } from 'dotenv'
+import User from '~/models/schemas/user.schema'
 
 // help me use file .env
 config()
 
-console.log(process.env.DB_USERNAME)
 const uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@twitter.z2myd.mongodb.net/?retryWrites=true&w=majority&appName=Twitter`
 
 // Create a MongoClient and connect to MongoDB by class constructor
 class DatabaseService {
   private client
+  private db
   constructor() {
     this.client = new MongoClient(uri)
+    this.db = this.client.db(process.env.DB_NAME)
   }
   async connect() {
     try {
       // Send a ping to confirm a successful connection
-      await this.client.db('admin').command({ ping: 1 })
+      await this.db.command({ ping: 1 })
       console.log('Pinged your deployment. You successfully connected to MongoDB!')
     } catch (err) {
       console.dir('Failed to ping your deployment. Please check your MongoDB connection details.', err)
       throw err
-    } finally {
-      // Ensures that the client will close when you finish/error
-      await this.client.close()
     }
+    // finally {
+    //    Ensures that the client will close when you finish/error
+    //   await this.client.close()
+    // }
+  }
+
+  get users(): Collection<User> {
+    return this.db.collection(process.env.DB_COLLECTION as string)
   }
 }
 
