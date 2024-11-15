bc23e0ffe66e2fcd84427fc1d421f988198e39c4
Phan Van Hung <Hungphanhung8@gmail.com>
2024-11-15 10:56:49 +0700

    Twitter: Install express-validator and plugin checkSchema for register p2

--- twitter/src/middlewares/users.middlewares.ts
+++ twitter/src/middlewares/users.middlewares.ts
@@ -1,6 +1,9 @@
 import { Request, Response, NextFunction } from 'express'
 import { checkSchema } from 'express-validator'
 
+// components
+import { validate } from '~/utils/validation'
+
 export const loginValidator = (req: Request, res: Response, next: NextFunction): any => {
   const { username, password } = req.body
   if (!username || !password) {
@@ -10,69 +13,81 @@ export const loginValidator = (req: Request, res: Response, next: NextFunction):
   }
 }
 
-export const registerValidator = checkSchema({
-  email: {
-    isEmail: true,
-    notEmpty: true,
-    trim: true
-  },
-  name: {
-    notEmpty: true,
-    isString: true,
-    isLength: {
-      options: {
-        min: 1,
-        max: 50
+export const registerValidator = validate(
+  checkSchema({
+    email: {
+      isEmail: true,
+      notEmpty: true,
+      trim: true
+    },
+    name: {
+      notEmpty: true,
+      isString: true,
+      isLength: {
+        options: {
+          min: 1,
+          max: 50
+        }
+      },
+      trim: true,
+      errorMessage: 'Please enter name'
+    },
+    password: {
+      notEmpty: true,
+      isString: true,
+      isLength: {
+        options: {
+          min: 8,
+          max: 50
+        }
+      },
+      trim: true,
+      isStrongPassword: {
+        options: {
+          minLength: 8,
+          minLowercase: 1,
+          minUppercase: 1,
+          minNumbers: 1,
+          minSymbols: 1
+        },
+        errorMessage:
+          'Password must be at least 8 characters long and contain at least one uppercase letter, one lowercase letter, one number, and one symbol'
       }
     },
-    trim: true
-  },
-  password: {
-    notEmpty: true,
-    isString: true,
-    isLength: {
-      options: {
-        min: 8,
-        max: 50
+    confirm_password: {
+      notEmpty: true,
+      isString: true,
+      isLength: {
+        options: {
+          min: 8,
+          max: 50
+        }
+      },
+      trim: true,
+      isStrongPassword: {
+        options: {
+          minLength: 8,
+          minLowercase: 1,
+          minUppercase: 1,
+          minNumbers: 1,
+          minSymbols: 1
+        },
+        errorMessage:
+          'Password must be at least 8 characters long and contain at least one uppercase letter, one lowercase letter, one number, and one symbol'
+      },
+      custom: {
+        // Dùng để so sánh form password có match form confirm password hay không?
+        options: (value, { req }) => value === req.body.password,
+        errorMessage: 'Passwords do not match'
       }
     },
-    trim: true,
-    isStrongPassword: {
-      options: {
-        minLength: 8,
-        minLowercase: 1,
-        minUppercase: 1,
-        minNumbers: 1,
-        minSymbols: 1
+    date_of_birth: {
+      isISO8601: {
+        options: {
+          strict: true,
+          strictSeparator: true
+        }
       }
     }
-  },
-  confirm_password: {
-    notEmpty: true,
-    isString: true,
-    isLength: {
-      options: {
-        min: 8,
-        max: 50
-      }
-    },
-    trim: true,
-    isStrongPassword: {
-      options: {
-        minLength: 8,
-        minLowercase: 1,
-        minUppercase: 1,
-        minNumbers: 1,
-        minSymbols: 1
-      }
-    }
-  },
-  date_of_birth: {
-    isISO8601: {
-      options: {
-        strict: true,
-        strictSeparator: true
-      }
-    }
-  }
-})
+  })
+)
