f11cca426ceed8f80e51a9e973dc16b80e4034b5
Phan Van Hung <Hungphanhung8@gmail.com>
2024-11-17 11:38:53 +0700

    Twitter: install jwt and sha256 to hash passwork

--- twitter/src/services/users.services.ts
+++ twitter/src/services/users.services.ts
@@ -1,11 +1,12 @@
-import { RegisterBodyReq } from '~/models/requests/User.requests'
 import User from '~/models/schemas/user.schema'
 import dbService from '~/services/database.services'
+import { RegisterBodyReq } from '~/models/requests/User.requests'
+import { hashPassword } from '~/utils/crypto'
 
 class UserService {
   async createUser(payload: RegisterBodyReq) {
     const result = await dbService.users.insertOne(
-      new User({ ...payload, date_of_birth: new Date(payload.date_of_birth) })
+      new User({ ...payload, date_of_birth: new Date(payload.date_of_birth), password: hashPassword(payload.password) })
     )
     return result
   }
---
+++ twitter/src/utils/crypto.ts
@@ -0,0 +1,10 @@
+import { createHash } from 'node:crypto'
+
+export function sha256(content: string) {
+  return createHash('sha256').update(content).digest('hex')
+}
+
+export function hashPassword(password: string) {
+  // ADD key secret to increase security
+  return sha256(password + process.env.KEY_SECRET_PW)
+}
