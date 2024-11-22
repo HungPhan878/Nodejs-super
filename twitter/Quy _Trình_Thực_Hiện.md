58d8605103515c8fbcd56e6f1e06cad2ec11f568
Phan Van Hung <Hungphanhung8@gmail.com>
2024-11-22 21:37:09 +0700

    Twitter: Added the description for routes.ts and rewrite logic of add a collection

---
+++ twitter/Quy _Trình_Thực_Hiện.md
@@ -0,0 +1,7 @@
+## Logic đã có ở trên figma
+
+## Description thêm các bước thêm một chức năng hay collection:
+
+1. thêm schema(interface, class) cho collector đó.
+2. Vào db.ts kết nối với collection đó.
+3. sử dụng collection vừa kết nối cho đúng như thêm vào bên user.services hay bên controller
--- twitter/src/routes/user.routes.ts
+++ twitter/src/routes/user.routes.ts
@@ -8,7 +8,39 @@ import { wrapRequestHandler } from '~/utils/wrapRequestHandler'
 
 const userRouter = Router()
 
+/**
+ * Description: Login a user
+ * Path: /login
+ * Method: POST
+ * Body:{
+ *      email: string,
+ *      password: string
+ *      }
+ * */
 userRouter.post('/login', loginValidator, wrapRequestHandler(loginController))
+
+/**
+ * Description: Register a new user
+ * Path: /register
+ * Method: POST
+ * Body:{
+ *    confirm-password: string (required),
+ *    password: string (required),
+ *    email: string ,
+ *    name: string ,
+ *    day of birth: string ,
+ * }
+ */
 userRouter.post('/register', registerValidator, wrapRequestHandler(registerController))
+
+/**
+ *  Description: Logout an user
+ *  Path: /logout
+ *  Method: POST
+ *  Body:{
+ *       refresh_token:string
+ *      }
+ */
+userRouter.post('/logout', registerValidator, wrapRequestHandler(registerController))
 
 export default userRouter
