0da39bf298101bebc379158c6f2772c60f914c66
Phan Van Hung <Hungphanhung8@gmail.com>
2024-12-15 15:03:34 +0700

    Twitter: Filter upload a image

--- twitter/src/controllers/medias.controllers.ts
+++ twitter/src/controllers/medias.controllers.ts
@@ -1,18 +1,9 @@
 import { Request, Response, NextFunction } from 'express'
-import formidable from 'formidable'
-import path from 'path'
-export const uploadSingleImageController = (req: Request, res: Response) => {
-  // const formidable = (await import('formidable')).default
-  const form = formidable({
-    uploadDir: path.resolve('uploads'),
-    keepExtensions: true,
-    maxFiles: 1,
-    maxFileSize: 300 * 1024
-  })
-  form.parse(req, async (err, fields, files) => {
-    if (err) {
-      throw err
-    }
-    res.status(200).json({ message: 'Image uploaded successfully' })
+import { handleUploadSingleImage } from '~/utils/file'
+export const uploadSingleImageController = async (req: Request, res: Response) => {
+  const data = await handleUploadSingleImage(req)
+  res.status(200).json({
+    message: 'Image uploaded successfully',
+    result: data
   })
 }
--- twitter/src/utils/file.ts
+++ twitter/src/utils/file.ts
@@ -1,5 +1,7 @@
 import fs from 'fs'
 import path from 'path'
+import { Request, Response } from 'express'
+import formidable from 'formidable'
 
 export const initFolder = () => {
   // fs is module in Node.js to do work with file system and folder, check paths exist or CRUD files
@@ -9,4 +11,35 @@ export const initFolder = () => {
       recursive: true // have nested files
     })
   }
+}
+
+export const handleUploadSingleImage = async (req: Request) => {
+  // const formidable = (await import('formidable')).default
+  const form = formidable({
+    uploadDir: path.resolve('uploads'),
+    keepExtensions: true,
+    maxFiles: 1,
+    maxFileSize: 300 * 1024, //300kb
+    filter: function ({ name, originalFilename, mimetype }) {
+      const valid = name === 'image' && Boolean(mimetype?.includes('image/'))
+      if (!valid) {
+        //Only use form.emit is res err
+        form.emit('error' as any, new Error('file type is not valid') as any)
+      }
+      return valid
+    }
+  })
+  return new Promise((resolve, reject) => {
+    form.parse(req, async (err, fields, files) => {
+      if (err) {
+        return reject(err)
+      }
+      // If the file of image is empty
+      // eslint-disable-next-line no-extra-boolean-cast
+      if (!Boolean(files.image)) {
+        return reject(new Error('No image file found'))
+      }
+      resolve(files)
+    })
+  })
 }
