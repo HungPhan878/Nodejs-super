1f722116ae7240d19fbab073f7e7ab264b080e4c
Phan Van Hung <Hungphanhung8@gmail.com>
2025-01-20 12:10:00 +0700

    Twitter: Fix error when not send email and refactor default err handler

--- twitter/src/middlewares/error.middlewares.ts
+++ twitter/src/middlewares/error.middlewares.ts
@@ -4,23 +4,31 @@ import HTTP_STATUS from '~/constants/httpStatusCode'
 import { ErrorWithStatus } from '~/models/Errors'
 
 const defaultErrorHandler: ErrorRequestHandler = (err, req, res, next) => {
-  if (err instanceof ErrorWithStatus) {
-    res.status(err.status).json(omit(err, 'status'))
-    return
+  try {
+    if (err instanceof ErrorWithStatus) {
+      res.status(err.status).json(omit(err, 'status'))
+      return
+    }
+
+    // Return an error when progress logic on server throw errors
+    const finalError: any = {}
+    Object.getOwnPropertyNames(err).forEach((key) => {
+      if (
+        !Object.getOwnPropertyDescriptor(err, key)?.configurable ||
+        !Object.getOwnPropertyDescriptor(err, key)?.writable
+      ) {
+        return
+      }
+      finalError[key] = err[key]
+    })
+    res
+      .status(HTTP_STATUS.SERVER_ERROR)
+      .json({ message: err.message, error_info: omit(err, 'status', 'stack') })
+  } catch (error) {
+    res
+      .status(HTTP_STATUS.SERVER_ERROR)
+      .json({ message: 'Internal server error', error_info: omit(error as any, 'status', 'stack') })
   }
-
-  // Return an error when progress logic on server throw errors
-  const descriptions = Object.getOwnPropertyDescriptors(err)
-
-  // Use for to loop over objects
-  for (const key in descriptions) {
-    descriptions[key].enumerable = true
-  }
-  Object.defineProperties(err, descriptions)
-
-  res
-    .status(HTTP_STATUS.SERVER_ERROR)
-    .json({ message: err.message, error_info: omit(err, 'status', 'stack') })
 }
 
 export default defaultErrorHandler
