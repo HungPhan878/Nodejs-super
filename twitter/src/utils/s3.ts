c0ecaecc1ad562239229657791743f6f999b3a4f
Phan Van Hung <Hungphanhung8@gmail.com>
2025-01-22 12:01:01 +0700

    Twitter: Test upload file to S3 and show image on website

--- twitter/src/utils/s3.ts
+++ twitter/src/utils/s3.ts
@@ -1,5 +1,9 @@
 import { S3 } from '@aws-sdk/client-s3'
+import { Upload } from '@aws-sdk/lib-storage'
 import { config } from 'dotenv'
+import fs from 'fs'
+import path from 'path'
+
 config()
 
 const s3 = new S3({
@@ -10,4 +14,27 @@ const s3 = new S3({
   }
 })
 
-s3.listBuckets({}).then((data) => console.log(data))
+// listBuckets use to test connect S3 success or failure
+// s3.listBuckets({}).then((data) => console.log(data))
+const file = fs.readFileSync(path.resolve('uploads/images/temp/investor.jpeg'))
+const parallelUploads3 = new Upload({
+  client: s3,
+  params: {
+    Bucket: 'twitter-clone-2025-ap-southeast-1',
+    Key: 'anh.jpg', // Change name to 'anh.jpg when uploaded
+    Body: file,
+    ContentType: 'image/jpeg' // Use to show image on website instead download
+  },
+  tags: [
+    /*...*/
+  ], // optional tags
+  queueSize: 4, // optional concurrency configuration
+  partSize: 1024 * 1024 * 5, // optional size of each part, in bytes, at least 5MB
+  leavePartsOnError: false // optional manually handle dropped parts
+})
+parallelUploads3.on('httpUploadProgress', (progress) => {
+  console.log(progress)
+})
+parallelUploads3.done().then((res) => {
+  console.log(res)
+})
